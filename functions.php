8bd16e01dd457f7f7962008947254c719e2457ef
Henry Zhang <henryzhang9802@gmail.com>
2019-07-17 22:17:29 +0100

    Add header and apply css

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php 
+
+    function material_minimal_resources() {
+        wp_enqueue_style('style', get_stylesheet_uri());
+    }
+
+    add_action('wp_enqueue_scripts', 'material_minimal_resources');
+
+    ?>
--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<h1>hello world!</h1>
-
 <?php 
 
 get_header();
@@ -7,8 +5,10 @@ get_header();
 if(have_posts()):
     while(have_posts()): the_post(); ?>
     
+    <div class="block">
     <h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
     <?php the_excerpt();?>
+    </div>
 
     
     <?php
