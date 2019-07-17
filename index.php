8da656ac986fb841a94a14f3313e73be8d226593
Henry Zhang <henryzhang9802@gmail.com>
2019-07-17 21:52:44 +0100

    Basic post display

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<h1>Hello World!</h1>
+<h1>hello world!</h1>
+
+<?php 
+
+if(have_posts()):
+    while(have_posts()): the_post(); ?>
+    
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
+    <?php the_content();?>
+
+    
+    <?php
+    endwhile;
+endif;
+
+?>
