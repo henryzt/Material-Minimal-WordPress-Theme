b2c2944a08e7ea00b6c1b789959a630386657e45
Henry Zhang <henryzhang9802@gmail.com>
2019-07-17 22:25:46 +0100

    Add footer

--- footer.php
+++ footer.php
@@ -1,3 +1,11 @@
-</div>
+    </div>
+
+    <footer class="site-footer">
+        <div class="footer">
+            <?php bloginfo("name"); ?>  -  &copy; <?php echo date("Y"); ?>
+        </div>
+
+    </footer>
+
 </body>
 </html>
