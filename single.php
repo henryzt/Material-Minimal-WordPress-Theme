3a1a36e9748bac20da48ef3620154a4a8647c0f9
Henry Zhang <henryzhang9802@gmail.com>
2019-07-18 18:13:59 +0100

    Add single post view

---
+++ single.php
@@ -0,0 +1,14 @@
+<?php 
+
+get_header();
+?>
+    
+    <div class="block">
+    <h2><a href="<?php the_post(); the_permalink(); ?>"><?php the_title();?></a></h2>
+    <?php the_content();?>
+    </div>
+
+    
+    <?php
+get_footer();
+?>
