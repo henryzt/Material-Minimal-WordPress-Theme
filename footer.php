8a2a54804db3850f5265d8d32758541d1fcc3abe
Henry Zhang <henryzhang9802@gmail.com>
2019-07-19 16:32:07 +0100

    Add copyright

--- footer.php
+++ footer.php
@@ -2,7 +2,8 @@
 
     <footer class="site-footer">
         <div class="footer">
-            <?php bloginfo("name"); ?>  -  &copy; <?php echo date("Y"); ?>
+            <?php bloginfo("name"); ?>  -  &copy; <?php echo date("Y"); ?> | Theme 
+            <a href="https://github.com/henryz00/Material-Minimal-WordPress-Theme">Material Minimal</a> by <a href="https://henryz.cc">Henry Zhang</a>
         </div>
 
     </footer>
