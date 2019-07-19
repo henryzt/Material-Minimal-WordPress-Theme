6d8feadbc3cbb4889e57f0dd860a7ac38bfe4e9f
Henry Zhang <henryzhang9802@gmail.com>
2019-07-19 16:25:26 +0100

    Add zoom effect

--- index.php
+++ index.php
@@ -3,16 +3,17 @@
 get_header();
 
 if(have_posts()):
-    while(have_posts()): the_post(); ?>
+    $IdCounter = 0;
+    while(have_posts()): the_post(); $IdCounter++;?>
     
-    <div class="block post_list" id="<?php get_the_ID(); ?>">
+    <div class="block post_list" id="<?php echo $IdCounter; ?>">
         <?php if(has_post_thumbnail()):?>
             <div style="width:40%;min-width:40%;padding:20px;">
                 <img src="<?php the_post_thumbnail_url(); ?>">
             </div>
         <?php endif; ?>
         <div style="padding:20px;">
-            <h2 class="post_title" onclick="change_zoom_css(<?php get_the_ID(); ?>);"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
+            <h2 class="post_title" onclick="change_zoom_css(<?php echo $IdCounter; ?>);"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
             <?php the_excerpt();?>
         </div>
     </div>
